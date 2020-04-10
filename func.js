b0b98fe0c4f47b22d03e7ca2c1b954960c75ba9e
keeplearninmylife <3347678815@qq.com>
2020-04-10 20:21:46 +0800

    function-

---
+++ func.js
@@ -0,0 +1,6 @@
+var add = function (a, b) {
+    if (b == 0)
+        return a;
+    return a + b;
+};
+add(2, 3);
---
+++ func.ts
@@ -0,0 +1,6 @@
+const add = (a:number, b?:number):number=>{
+    if (b == 0 ) return a
+    return a+b
+}
+
+add (2,3)
