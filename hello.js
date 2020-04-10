e36fff491179eef269327be943c18ae12007430e
keeplearninmylife <3347678815@qq.com>
2020-04-10 20:00:16 +0800

    ts类型与interface

---
+++ hello.js
@@ -0,0 +1,7 @@
+var hello = function (name) {
+    console.log("hello," + name);
+};
+hello("ts");
+var jack = {
+    name: "jack"
+};
---
+++ hello.ts
@@ -0,0 +1,17 @@
+const hello = (name:string) :any=>{
+    console.log (`hello,${name}`)
+}
+
+hello("ts")
+
+
+
+interface Person {
+    name: string;
+      age ? : number
+}
+
+let jack = {
+    name:"jack",
+    // age:12
+}
