4cb7ed1301960dc6d3b96630bd4dd4f2a7526a6a
keeplearninmylife <3347678815@qq.com>
2020-04-10 20:57:15 +0800

    类的继承，方法的重写

--- classLearn.ts
+++ classLearn.ts
@@ -13,3 +13,22 @@ snake.run()
 console.log(snake.name)
 snake.name="lily"
 console.log(snake.name)
+
+
+//类的继承与方法的重写
+class Dog extends Animal{
+    constructor(name){
+        super(name)
+        console.log(this.name)
+    }
+    run(){
+        console.log(`${this.name},快跑啊`)
+    }
+    dark(){
+        console.log("汪汪汪")
+    }
+}
+const dog =new Dog("旺财")
+dog.run()
+dog.dark()
+
